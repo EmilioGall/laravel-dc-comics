c61d9b9cab1ff5137e229c7717e2efbc48ec41f2
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-15 18:01:13 +0200

    feat: added actions buttons (modify comic, delete comic and create new comic) - comics/index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,11 +1,34 @@
-@extends('_layouts.admin');
+@extends('_layouts.admin')
 
 @section('content')
    <main>
 
       <div class="container-md">
 
-         <h1 class="text-primary text-center">Comics Archive</h1>
+         <div class="row justify-content-between my-3">
+
+            <div class="col-10">
+
+               <h1 class="text-primary text-center m-0">Comics Archive</h1>
+
+            </div>
+
+
+            <div class="col-2 d-flex justify-content-center align-items-end">
+
+               <button type="button" class="btn btn-primary h-75 w-100 d-flex align-items-center justify-content-center">
+
+                  <a href="{{ route('comics.create') }}">
+
+                     <i class="fa-solid fa-plus"></i>
+
+                  </a>
+
+               </button>
+
+            </div>
+
+         </div>
 
          <section class="row">
 
@@ -19,6 +42,7 @@
                      <th scope="col">Type</th>
                      <th scope="col">Series</th>
                      <th scope="col">Price</th>
+                     <th scope="col">Actions</th>
                   </tr>
 
                </thead>
@@ -32,6 +56,29 @@
                            <td>{{ $comic->type }}</td>
                            <td>{{ $comic->series }}</td>
                            <td>{{ $comic->price }} $</td>
+                           <td>
+
+                              <button type="button" class="btn btn-outline-primary">
+
+                                 <a href="">
+               
+                                    <i class="fa-regular fa-pen-to-square"></i>
+               
+                                 </a>
+               
+                              </button>
+
+                              <button type="button" class="btn btn-outline-danger">
+
+                                 <a href="">
+               
+                                    <i class="fa-regular fa-trash-can"></i>
+
+                                 </a>
+               
+                              </button>
+
+                           </td>
                         </div>
                      </tr>
                   @endforeach
